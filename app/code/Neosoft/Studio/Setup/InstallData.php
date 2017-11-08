76297435302c022973a0eecc253a464b4ee2c0b8
DurgeshParmar <durgesh.parmar@e-zest.in>
2017-11-08 12:03:00 +0530

    git command testing by pushing default magento2 code base

---
+++ app/code/Neosoft/Studio/Setup/InstallData.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace Neosoft\Studio\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'uploadable',
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Uploadable',
+                'input' => 'boolean',
+                'class' => '',
+                'source' => '',
+                'global' => 1,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => true,
+                'default' => null,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => '',
+                'system' => 1,
+                'group' => 'General',
+                'option' => array('values' => array(""))
+            ]
+        );
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'designable',
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Designable',
+                'input' => 'boolean',
+                'class' => '',
+                'source' => '',
+                'global' => 1,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => true,
+                'default' => null,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => '',
+                'system' => 1,
+                'group' => 'General',
+                'option' => array('values' => array(""))
+            ]
+        );
+    }
+}
---
+++ app/code/Neosoft/Studio/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Neosoft_Studio',
+    __DIR__
+);
